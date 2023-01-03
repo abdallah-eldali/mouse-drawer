1bdf8d2199d561fcdac8dde666ff47dd62f5652e
abdallah-eldali <abdallahed19@gmail.com>
2023-01-03 00:34:57 -0500

    it seems that drawing through the GUI's window isn't a good idea. It causes it stop responding due to sheer amount of clicks that are being done through the transparent section of the window. Instead, we should close the window and start drawing

--- gui.py
+++ gui.py
@@ -3,9 +3,18 @@ from PIL import ImageTk, Image
 from io import BytesIO
 from time import sleep
 from mouse import move
+from test2 import draw_from
+
+#TODO:
+# 1. Find a way to cancel the script while running
+# 2. Find a way to execute the code
+# 3. Overall, make this work with the mouse script
+# 4. Make sure to add an option to import your own images 
+#    that will execute black-white.py -> display the newly created png -> call mouse script to start drawing it
+
 
 byte_io = BytesIO()                     #this is used to save the image in a ByteIO and pass it to the draw_image function
-img = Image.open('muT5j.png')
+img = Image.open('result_bw.png')
 img.save(byte_io, format=img.format)
 width, height = img.size
 
@@ -21,6 +30,8 @@ window = sg.Window('Window Title', layout, transparent_color='red', alpha_channe
 # print(window['graph'].get_size())
 img_id = window['graph'].draw_image(data=byte_io.getvalue(), location=(0, 0))
 
+# Flag to know if we should draw or not
+start_drawing = False
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
@@ -30,17 +41,26 @@ while True:
     if event == 'Ok':   
         # window['graph'].update(background_color='red', visible=False)
         # window['graph'].tk_canvas.itemconfig(img, transparentcolor='red')
-        window['graph'].delete_figure(img_id)
-        # sleep(1)
-        wx, wy = window.current_location()
-        graph_widget = window['graph'].widget
-        gx, gy = graph_widget.winfo_rootx(), graph_widget.winfo_rooty()
-        print(gx, gy)
+        # window['graph'].delete_figure(img_id)
+
+        # wx, wy = window.current_location()
+        # graph_widget = window['graph'].widget
+        # #get the location of the canvas in the screen
+        # gx, gy = graph_widget.winfo_rootx(), graph_widget.winfo_rooty()
+        # print(gx, gy)
 
 
-        move(gx, gy)
-        #get the location of the canvas in the screen
+        # sleep(0.5)
+        # draw_from(gx+(offset/2), gy+(offset/2))
+        # draw_from(gx, gy)
+        start_drawing = True
+        break
 
-        
+
+
+graph_widget = window['graph'].widget
+gx, gy = graph_widget.winfo_rootx(), graph_widget.winfo_rooty()
 
 window.close()
+if start_drawing:
+    draw_from(gx, gy)
